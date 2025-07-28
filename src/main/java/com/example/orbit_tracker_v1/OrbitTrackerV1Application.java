26e8124a1ee4262a664e2db761404d3fb038a3ce
Tegge <John.Tegge@amentumcms.com>
2025-07-28 13:23:10 -0600

    first-commit

---
+++ src/main/java/com/example/orbit_tracker_v1/OrbitTrackerV1Application.java
@@ -0,0 +1,13 @@
+package com.example.orbit_tracker_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrbitTrackerV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrbitTrackerV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/orbit_tracker_v1/OrbitTrackerV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.orbit_tracker_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrbitTrackerV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
